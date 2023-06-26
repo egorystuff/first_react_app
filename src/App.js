44b9ef49e96bf40c474637d270f88410f9ae0dc9
Yahor Yankavets <mr.yankovec@gmail.com>
2023-06-26 14:56:34 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-	return <div className="App"></div>;
+	const [likes, setLikes] = useState(0);
+	const [value, setValue] = useState('Текст в инпуте');
+
+	function increment() {
+		setLikes(likes + 1);
+	}
+
+	function decrement() {
+		setLikes(likes - 1);
+	}
+
+	return (
+		<div className="App">
+			<h1>{likes}</h1>
+			<h1>{value}</h1>
+			<input type="text" value={value} onChange={(event) => setValue(event.target.value)}></input>
+
+			<button onClick={increment}>Incremetn</button>
+			<button onClick={decrement}>Decrement</button>
+		</div>
+	);
 }
 
 export default App;
