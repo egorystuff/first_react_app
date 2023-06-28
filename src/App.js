6fdeb7ce2221bbd347f86b3dea9944dd11f76f94
Yahor Yankavets <mr.yankovec@gmail.com>
2023-06-28 14:39:33 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
@@ -8,18 +8,36 @@ import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 
 function App() {
-	const [posts, setposts] = useState([
-		{ id: 1, title: 'JavaScript', body: 'Description' },
-		{ id: 2, title: 'JavaScript 2', body: 'Description' },
-		{ id: 3, title: 'JavaScript 3', body: 'Description' },
-	]);
+	const [posts, setPosts] = useState([{ id: 1, title: 'JavaScript', body: 'Description' }]);
+
+	const [post, setPost] = useState({ title: '', body: '' });
+
+	const addNewPost = (e) => {
+		e.preventDefault();
+
+		setPosts([...posts, { ...post, id: Date.now() }]);
+		setPost({ title: '', body: '' });
+	};
 
 	return (
 		<div className="App">
+			{/* Управляемый компонент */}
 			<form>
-				<MyInput type="text" placeholder="Название поста" />
-				<MyInput type="text" placeholder="Описание поста" />
-				<MyButton disabled>Создать пост</MyButton>
+				<MyInput
+					value={post.title}
+					onChange={(e) => setPost({ ...post, title: e.target.value })}
+					type="text"
+					placeholder="Название поста"
+				/>
+
+				<MyInput
+					value={post.body}
+					onChange={(e) => setPost({ ...post, body: e.target.value })}
+					type="text"
+					placeholder="Описание поста"
+				/>
+
+				<MyButton onClick={addNewPost}>Создать пост</MyButton>
 			</form>
 			<PostList posts={posts} title="Список постов 1" />
 		</div>
