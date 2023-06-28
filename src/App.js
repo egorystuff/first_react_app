8cdf42ad39b22be463401672cd3fef8fe7e83858
Yahor Yankavets <mr.yankovec@gmail.com>
2023-06-28 08:35:05 +0300

    update 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
 	const [posts, setposts] = useState([
@@ -10,12 +13,15 @@ function App() {
 		{ id: 2, title: 'JavaScript 2', body: 'Description' },
 		{ id: 3, title: 'JavaScript 3', body: 'Description' },
 	]);
+
 	return (
 		<div className="App">
-			<h1 style={{ textAlign: 'center' }}>Список постов</h1>
-			{posts.map((post) => (
-				<PostItem post={post} key={post.id} />
-			))}
+			<form>
+				<MyInput type="text" placeholder="Название поста" />
+				<MyInput type="text" placeholder="Описание поста" />
+				<MyButton disabled>Создать пост</MyButton>
+			</form>
+			<PostList posts={posts} title="Список постов 1" />
 		</div>
 	);
 }
