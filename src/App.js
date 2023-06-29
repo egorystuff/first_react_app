2a711340a6d660d76b8830958b449e21a4615a4e
Yahor Yankavets <mr.yankovec@gmail.com>
2023-06-29 18:09:14 +0300

    added new functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MySelect from './components/UI/select/MySelect';
 import MyInput from './components/UI/input/MyInput';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -14,6 +16,7 @@ function App() {
 	]);
 
 	const [filter, setFilter] = useState({ sort: '', query: '' });
+	const [modal, setModal] = useState(false);
 
 	const sortedPosts = useMemo(() => {
 		console.log('вызов ф-ции getSortedPosts');
@@ -31,6 +34,7 @@ function App() {
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
+		setModal(false);
 	};
 
 	// получаем пост из дочернего компонента
@@ -40,17 +44,16 @@ function App() {
 
 	return (
 		<div className="App">
-			<PostForm create={createPost} />
+			<MyButton style={{marginTop:30}} onClick={() => setModal(true)}> Создать пост</MyButton>
+			<MyModal visible={modal} setVisible={setModal}>
+				<PostForm create={createPost} />
+			</MyModal>
 
 			<hr style={{ margin: '15px 0' }} />
 
 			<PostFilter filter={filter} setFilter={setFilter} />
 
-			{sortedAndSearchedPosts.length ? (
-				<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов" />
-			) : (
-				<h1 style={{ textAlign: 'center' }}>Посты не найдены!</h1>
-			)}
+			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов" />
 		</div>
 	);
 }
