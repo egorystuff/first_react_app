0bdc61592978e4fa3a5b9838a88e9888b18af4f3
Yahor Yankavets <mr.yankovec@gmail.com>
2023-06-30 15:20:32 +0300

    added new functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
-import MySelect from './components/UI/select/MySelect';
-import MyInput from './components/UI/input/MyInput';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './hooks/usePosts';
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -17,20 +16,7 @@ function App() {
 
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
-
-	const sortedPosts = useMemo(() => {
-		console.log('вызов ф-ции getSortedPosts');
-
-		if (filter.sort) {
-			return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-		} else {
-			return posts;
-		}
-	}, [filter.sort, posts]);
-
-	const sortedAndSearchedPosts = useMemo(() => {
-		return sortedPosts.filter((post) => post.title.toLowerCase().includes(filter.query));
-	}, [filter.query, sortedPosts]);
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
@@ -44,7 +30,10 @@ function App() {
 
 	return (
 		<div className="App">
-			<MyButton style={{marginTop:30}} onClick={() => setModal(true)}> Создать пост</MyButton>
+			<MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+				{' '}
+				Создать пост
+			</MyButton>
 			<MyModal visible={modal} setVisible={setModal}>
 				<PostForm create={createPost} />
 			</MyModal>
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import MyButton from './/UI/button/MyButton';
 import MyInput from './/UI/input/MyInput';
 
